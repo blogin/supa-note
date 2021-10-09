98c1fd2c470de77729d03f4dcdfb7bb4976cd243
alogin2009@gmail.com <alogin2009@gmail.com>
2021-10-09 14:30:19 +0300

    1h break

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Archive from '../views/Archive.vue'
 import firebase from 'firebase';
 import {
   Message
@@ -23,8 +24,11 @@ const routes = [{
     component: Login
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/archive',
+    name: 'Archive',
+    meta: {
+      authReq: true
+    }
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -41,6 +45,7 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
+  debugger
   if (to.matched.some(record => record.meta.authReq)) {
     if (firebase.auth().currentUser) {
       next();
