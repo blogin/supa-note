c97c40389f5b824922e7fa5a22212cfc5c189d5e
alogin2009@gmail.com <alogin2009@gmail.com>
2021-10-09 09:32:35 +0300

    add login form

--- src/main.js
+++ src/main.js
@@ -2,8 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { initializeApp } from "firebase/app";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAX1eV5fGGu3E2-w-nriJuU4DSv9_Q0F8",
+  authDomain: "supa-note.firebaseapp.com",
+  databaseURL: "https://supa-note-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "supa-note",
+  storageBucket: "supa-note.appspot.com",
+  messagingSenderId: "171043286224",
+  appId: "1:171043286224:web:63db895503c2466e2d82d5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI);
 
 new Vue({
   router,
---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="form">
+      <el-input placeholder="Please input" v-model="login" class="inp"></el-input>
+      <el-input placeholder="Please input password" v-model="pass" show-password class="inp"></el-input>
+      <div class="btn-group">
+        <el-button :loading="true">LogIn</el-button>
+        <el-button type="info">Register</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      login:"",
+      pass:""
+    }
+  }
+}
+</script>
+
+<style scoped>
+.form{
+  width: 300px;
+  border: 1px solid lightgrey;
+  padding: 10px;
+  border-radius: 5px;
+}
+.inp{
+  margin: 5px 0;
+}
+</style>
