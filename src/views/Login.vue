4b4b7b9b110b8ccdb73895b117f0ab9b26c89488
пащенко павел петрович <pppashhe@mts.ru>
2021-10-11 10:04:54 +0300

    add keyup.enter to login form, centered login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
+  <div class="form-cont">
     <div class="form">
       <el-input
         placeholder="Please input"
         v-model="email"
         class="inp"
       ></el-input>
+      <!-- Обертка в div потому как у el-input нет возможности использовать @keyup.enter -->
+      <div @keyup.enter="check()">
       <el-input
         placeholder="Please input password"
         v-model="pass"
         show-password
         class="inp"
-      ><p @keyup.enter="check()" /></el-input>
+      ></el-input></div>
       <div class="btn-group">
         <el-button :loading="btnLoading" @click="check()" >LogIn</el-button>
         <el-button type="info" @click="register()" :loading="btnLoadingReg">Register</el-button>
@@ -54,11 +56,18 @@ export default {
 </script>
 
 <style scoped>
+.form-cont{
+  position: relative;
+  
+}
 .form {
   width: 300px;
   border: 1px solid lightgrey;
   padding: 10px;
   border-radius: 5px;
+  position: absolute;
+  top: 50%; left: 50%;
+  transform: translate(-50%,170%);
 }
 .inp {
   margin: 5px 0;
